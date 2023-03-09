2e81ca71e8d31ea001a931fdc688982dba0fb754
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-03-09 05:11:42 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,7 +5,8 @@ main=$(jq -r '.main // ""' package.json)
 types=$(jq -r '.types // ""' package.json)
 dist_main=$(jq -r '."dist-main" // ""' package.json)
 dist_types=$(jq -r '."dist-types" // ""' package.json)
-
+exports=$(jq -r '.exports // ""' package.json)
+dist_exports=$(jq -r '."dist-exports" // ""' package.json)
 # Get the first argument passed to the script as the npm lifecycle event
 npm_lifecycle_event=$1
 
@@ -20,7 +21,12 @@ if [ "$npm_lifecycle_event" == "prepublish" ]; then
   if [ -n "$types" ] && [ -n "$dist_types" ]; then
     jq --arg types "$types" --arg dist_types "$dist_types" \
       '.types = $dist_types | ."dist-types" = $types' package.json > tmp.json && mv tmp.json package.json
-  fi  
+  fi
+  # Rename the dist-exports field to exports in the package.json file if it is not empty
+  if [ -n "$dist_exports" ]; then
+    jq --arg dist_exports "$dist_exports" \
+      '.exports = $dist_exports | del(."dist-exports")' package.json > tmp.json && mv tmp.json package.json
+  fi
 elif [ "$npm_lifecycle_event" == "postpublish" ]; then
   # Swap back the values of main and dist-main in the package.json file if they are not empty
   if [ -n "$main" ] && [ -n "$dist_main" ]; then
@@ -31,7 +37,12 @@ elif [ "$npm_lifecycle_event" == "postpublish" ]; then
   if [ -n "$types" ] && [ -n "$dist_types" ]; then
     jq --arg types "$types" --arg dist_types "$dist_types" \
       '.types = $dist_types | ."dist-types" = $types' package.json > tmp.json && mv tmp.json package.json
-fi 
+  fi
+  # Rename the exports field to dist-exports in the package.json file if it is not empty
+  if [ -n "$exports" ]; then
+    jq --arg exports "$exports" \
+      '.exports = $exports | del(."dist-exports")' package.json > tmp.json && mv tmp.json package.json
+  fi
 else 
 # Exit with an error message if neither prepublish nor postpublish or no argument is given 
 echo "This script should only be run as prepublish or postpublish with an argument." 
