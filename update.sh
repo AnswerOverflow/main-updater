74c2f04c0ed093901e9aa027e284cf5ae446d821
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-03-09 04:28:47 -0500

    Swap to using dist fields

--- update.sh
+++ update.sh
@@ -1,24 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Get the current values of main and types
+# Get the current values of main, types, dist-main and dist-types
 main=$(jq -r '.main' package.json)
 types=$(jq -r '.types' package.json)
-
-# Define the new values for prepublish and postpublish
-prepublish_main="./dist/index.js"
-prepublish_types="./dist/index.d.ts"
-postpublish_main="index.ts"
-postpublish_types="index.ts"
+dist_main=$(jq -r '."dist-main"' package.json)
+dist_types=$(jq -r '."dist-types"' package.json)
 
 # Check if the script is run as prepublish or postpublish
 if [ "$npm_lifecycle_event" == "prepublish" ]; then
-  # Update the package.json with the new values for prepublish
-  jq --arg main "$prepublish_main" --arg types "$prepublish_types" \
-    '.main = $main | .types = $types' package.json > tmp.json && mv tmp.json package.json
+  # Swap the values of main and dist-main in the package.json file
+  jq --arg main "$main" --arg dist_main "$dist_main" \
+    '.main = $dist_main | ."dist-main" = $main' package.json > tmp.json && mv tmp.json package.json
 elif [ "$npm_lifecycle_event" == "postpublish" ]; then
-  # Update the package.json with the new values for postpublish
-  jq --arg main "$postpublish_main" --arg types "$postpublish_types" \
-    '.main = $main | .types = $types' package.json > tmp.json && mv tmp.json package.json
+  # Swap the values of types and dist-types in the package.json file
+  jq --arg types "$types" --arg dist_types "$dist_types" \
+    '.types = $dist_types | ."dist-types" = $types' package.json > tmp.json && mv tmp.json package.json
 else
   # Exit with an error message if neither prepublish nor postpublish
   echo "This script should only be run as prepublish or postpublish."
